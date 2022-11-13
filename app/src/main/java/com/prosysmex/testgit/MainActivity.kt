09363548020491a00a595b6871981e76efa56cbc
rmoralesgit <robertomoralesj@gmail.com>
2022-11-12 20:23:47 -0600

    segundo commit

--- app/src/main/java/com/prosysmex/testgit/MainActivity.kt
+++ app/src/main/java/com/prosysmex/testgit/MainActivity.kt
@@ -22,7 +22,8 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    //coment 12nov22
+                    Greeting("Android Kotlin")
                 }
             }
         }
